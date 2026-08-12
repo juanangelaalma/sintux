fa50d1876502df9a373e8a7da4ddc7eb4baace1c
Resky Surya <resky.surya@behaestex.co.id>
2026-08-12 11:01:50 +0700

    refactor: relocate module migrations into domain modules
    - Move central & tenant migrations into Modules/{Module}/database/migrations
    - Update config/tenancy.php migration_parameters path to dynamically discover module tenant migrations
    - Keep core/framework migrations in root database/migrations and COA in root tenant

--- config/tenancy.php
+++ config/tenancy.php
@@ -182,7 +182,10 @@ return [
      */
     'migration_parameters' => [
         '--force' => true, // This needs to be true to run migrations in production.
-        '--path' => [database_path('migrations/tenant')],
+        '--path' => array_merge(
+            [database_path('migrations/tenant')],
+            glob(base_path('Modules/*/database/migrations/tenant')) ?: []
+        ),
         '--realpath' => true,
     ],
 
